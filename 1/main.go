42e9de52b506a62f9fd499f34438d685966e17d2
tamir <tamir3837@gmail.com>
2022-12-04 23:58:48 +0200

    finish day 1 (bad code)

--- 1/main.go
+++ 1/main.go
@@ -1,11 +1,42 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 const (
 	fileName = "input.txt"
 )
 
 func main() {
-	file, err := os.OpenFile()
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var currentSum int = 0
+	var biggestSum int = 0
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			if biggestSum < currentSum {
+				biggestSum = currentSum
+			}
+			currentSum = 0
+		} else {
+			calorie, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				panic(err)
+			}
+
+			currentSum += calorie
+		}
+
+	}
+	fmt.Println(biggestSum)
 }
